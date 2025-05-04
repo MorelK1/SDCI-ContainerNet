64f9a143b3f315ea50057e02d02f8e917d23f10f
MorelK1 <kouhossounon.vianney@gmail.com>
2025-05-04 22:42:57 +0200

    `feat` test

--- mape.py
+++ mape.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from mininet.net import Containernet
 
 # Global variables
 DPID = 4 # central switch ID
@@ -8,6 +9,14 @@ THRESHOLD = 1200 # threshold for latency
 CHECK_INTERVAL = 2 # interval to check the system state
 
 adaptation_done = False # flag to indicate if adaptation is done
+
+def start_gi2(net: 'Containernet'):
+    """
+    Start the GI2 gateway.
+    """
+    gi2 = net.get('gi2')
+    gi2.cmd('node gateway.js --local_ip 10.0.0.12 --local_port 8182 --local_name gi2 --remote_ip 10.0.0.1 --remote_port 8080 --remote_name srv &')
+
 # MAPE loop functions
 
 def monitor():
@@ -43,12 +52,15 @@ def plan():
     """
     Plan function to determine necessary actions.
     """
-    print("[Planning...] Overload detected, Preparing to redirect to GI2.")
+    print("[Planning...] Overload detected, Preparing to start GI2 and to redirect traffic.")
 
-def execute():
+def execute(net: 'Containernet'):
     """
     Execute function to apply the planned actions.
     """
+    print("[Executing actions...] Starting new Gateway: GI2.")
+    # Start the GI2 gateway
+    start_gi2(net)
     print("[Executing actions...] Redirecting traffic to GI2 via ofctl_rest.")
     redirection_rule = {
         "dpid": DPID,
@@ -69,6 +81,10 @@ def execute():
         print("HTTP request successful:", response.status_code)
     except Exception as e:
         print("Error:", e)
+
+def mape_loop(net: 'Containernet'):
+    start_gi2(net)
+
 
 if __name__ == '__main__':
     # """
