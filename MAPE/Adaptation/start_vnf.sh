1c7542a11fde26cbdb51e71bf04fbca00e28fb21
MorelK1 <kouhossounon.vianney@gmail.com>
2025-05-23 10:49:44 +0200

    `feat` adaptation gateway

---
+++ MAPE/Adaptation/start_vnf.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /app
+
+node gateway.js \
+  --local_ip "$LOCAL_IP" \
+  --local_port "$LOCAL_PORT" \
+  --local_name "$LOCAL_NAME" \
+  --remote_ip "$REMOTE_IP" \
+  --remote_port "$REMOTE_PORT" \
+  --remote_name "$REMOTE_NAME" &
